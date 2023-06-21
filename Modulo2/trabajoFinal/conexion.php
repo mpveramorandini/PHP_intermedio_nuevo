576b25d5edbff463066e5e22c1f39f9ffe0f28cd
Maria Paula Vera <mariapaulavera@hotmail.com>
2023-06-21 17:38:41 -0300

    ok

--- Modulo2/trabajoFinal/conexion.php
+++ Modulo2/trabajoFinal/conexion.php
@@ -1,8 +1,8 @@
 <?php 
 
-//$conexion_db = mysqli_connect("localhost", "root", "", "php_intermedio_mpv");
+$conexion_db = mysqli_connect("localhost", "root", "", "php_intermedio_mpv");
 
 
 
-$conexion_db = mysqli_connect("localhost", "id20761485_mariapaulavera", "/?G-+d?QS*S=l30s", "id20761485_phpintermedio");
+//$conexion_db = mysqli_connect("localhost", "id20761485_mariapaulavera", "/?G-+d?QS*S=l30s", "id20761485_phpintermedio");
  
--- Modulo2/unidad4_modulo2/cargar_alumno.php
+++ Modulo2/unidad4_modulo2/cargar_alumno.php
@@ -16,22 +16,20 @@ if ($codigo_captcha == $_SESSION['codigo_captcha']) {
     $tipo_img = $_FILES['imagen']['type'];
     $tmp_img = $_FILES['imagen']['tmp_name'];
 
-
-
-
     $destino = 'imagenes/'. $nombre_img;
 
-    if (($tipo_img != "image/jpeg" &&  $tipo_img != "image/jpg" &&  $tipo_img != "image/png" &&  $tipo_img != "image/gif") or $tamanio_img >400000) {
-        header("Location: cargar.php?error");
+
+     if (($tipo_img != "image/jpeg" &&  $tipo_img != "image/jpg" &&  $tipo_img != "image/png" &&  $tipo_img != "image/gif") or $tamanio_img >400000) {
+         header("Location: cargar.php?error");
     } else {
-        move_uploaded_file($tmp_img, $destino);
+         move_uploaded_file($tmp_img, $destino);
         mysqli_query($conexion_db, "INSERT INTO alumnos VALUES (DEFAULT,'$nombre_alumno','$apellido_alumno','$nombre_img', '$descripcion_alumno', '$estado_alumno')");
-        header("Location:cargar.php?ok");
-    }
-} else {
-    header("Location:cargar.php?error_codigo");
+         header("Location:cargar.php?ok");
+     }
+ } else {
+     header("Location:cargar.php?error_codigo");
 }
-mysqli_close($conexion_db);
+ mysqli_close($conexion_db);
 
 
 //  move_uploaded_file($tmp_img , $destino);
